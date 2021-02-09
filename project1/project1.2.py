7c52ad3737ddcb76f8fb47cefa48e711bf674ec3
Daniel Nakhimovich <dnahimov@gmail.com>
2021-02-09 15:55:43 -0500

    new imports

--- project1/project1.2.py
+++ project1/project1.2.py
@@ -1,23 +1,26 @@
+import sys
+sys.path.append('../sim_module')
 import sim
 
-
-OBSTACLE_MASTERLIST=['Cylinder','Cylinder0','Cylinder1','Cylinder2','Cuboid','Cuboid0','Cuboid1','Cuboid2']
-print ('Program started')
-sim.simxFinish(-1) # just in case, close all opened connections
-clientID=sim.simxStart('127.0.0.1',19997,True,True,5000,5) # Connect to CoppeliaSim
-if clientID!=-1:
-    print ('Connected to remote API server')
+OBSTACLE_MASTERLIST = ['Cylinder', 'Cylinder0', 'Cylinder1', 'Cylinder2', 'Cuboid', 'Cuboid0', 'Cuboid1', 'Cuboid2']
+print('Program started')
+sim.simxFinish(-1)  # just in case, close all opened connections
+clientID = sim.simxStart('127.0.0.1', 19997, True, True, 5000, 5)  # Connect to CoppeliaSim
+if clientID != -1:
+    print('Connected to remote API server')
     # load the scene
-    res=sim.simxLoadScene(clientID,'scene1.2.ttt',True,sim.simx_opmode_blocking)
+    res = sim.simxLoadScene(clientID, 'scene1.2.ttt', True, sim.simx_opmode_blocking)
 
     #get object information
     # res, objs = sim.simxGetObjects(clientID, sim.sim_handle_all, sim.simx_opmode_blocking)
-    res, handles, intData, floatData, names=sim.simxGetObjectGroupData(clientID,sim.sim_appobj_object_type,0,sim.simx_opmode_blocking)
+    res, handles, intData, floatData, names = sim.simxGetObjectGroupData(
+        clientID, sim.sim_appobj_object_type, 0, sim.simx_opmode_blocking
+    )
 
     #get object handles and names
-    robot_names=[[i,n] for i,n in enumerate(names) if 'dr20'==n] #might need to do if dr20 IN n
-    obstacle_names=[[i,n] for i,n in enumerate(names) if n in OBSTACLE_MASTERLIST]
-    start_dummy=[[i,n] for i,n in enumerate(names) if n=='Start']
+    robot_names = [[i, n] for i, n in enumerate(names) if 'dr20' == n]  #might need to do if dr20 IN n
+    obstacle_names = [[i, n] for i, n in enumerate(names) if n in OBSTACLE_MASTERLIST]
+    start_dummy = [[i, n] for i, n in enumerate(names) if n == 'Start']
     end_dummy = [[i, n] for i, n in enumerate(names) if n == 'End']
     print(robot_names)
     print(obstacle_names)
@@ -33,5 +36,5 @@ if clientID!=-1:
     searchCount = 2  # how many times OMPL will run for a given task
 
 # Stop simulation:
-sim.simxStopSimulation(clientID,sim.simx_opmode_oneshot_wait)
+sim.simxStopSimulation(clientID, sim.simx_opmode_oneshot_wait)
 sim.simxFinish(clientID)
